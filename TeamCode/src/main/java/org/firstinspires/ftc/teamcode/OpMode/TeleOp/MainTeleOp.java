71b5a5057caddf3e0d871658e357eeb91e0c3a1c
chris.32987 <ingrassiachristopher@gmail.com>
2026-03-26 14:41:17 +0000

    tested code worked

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpMode/TeleOp/MainTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpMode/TeleOp/MainTeleOp.java
@@ -1,24 +1,28 @@
 package org.firstinspires.ftc.teamcode.OpMode.TeleOp;
 
-import org.firstinspires.ftc.teamcode.pedroPathing.Constants;
+//import org.firstinspires.ftc.teamcode.pedroPathing.Constants;
 import org.firstinspires.ftc.teamcode.subsystems.Intake;
 import org.firstinspires.ftc.teamcode.subsystems.Shooter;
-
+//import org.firstinspires.ftc.teamcode.subsystems.Shooter;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import dev.nextftc.core.components.BindingsComponent;
 import dev.nextftc.core.components.SubsystemComponent;
-import dev.nextftc.extensions.pedro.PedroComponent;
+//import dev.nextftc.extensions.pedro.PedroComponent;
 import dev.nextftc.ftc.Gamepads;
 import dev.nextftc.ftc.NextFTCOpMode;
 import dev.nextftc.ftc.components.BulkReadComponent;
 
+@TeleOp(name = "MainTeleOp", group = "TeleOp")
 public class MainTeleOp extends NextFTCOpMode {
     public MainTeleOp() {
         addComponents(
                 BulkReadComponent.INSTANCE,
                 BindingsComponent.INSTANCE,
-                new SubsystemComponent(Intake.INSTANCE,),
-                new PedroComponent(Constants::createFollower)
+                new SubsystemComponent(Intake.INSTANCE)
+                //new SubsystemComponent(Shooter.INSTANCE)
+                //new PedroComponent(Constants::createFollower)
         );
+
     }
     @Override public void onInit() { }
     @Override public void onWaitForStart() { }
@@ -29,9 +33,9 @@ public class MainTeleOp extends NextFTCOpMode {
         Gamepads.gamepad1().leftBumper()
                 .whenBecomesTrue(Intake.INSTANCE.intakeReverse)
                 .whenBecomesFalse(Intake.INSTANCE.intakeOff);
-        Gamepads.gamepad1().rightTrigger().greaterThan(0.05)
-                .whenBecomesTrue(Shooter.INSTANCE.FlywheelOn)
-                .whenBecomesFalse(Shooter.INSTANCE.FlywheelOff);
+        //Gamepads.gamepad1().rightTrigger().greaterThan(0.05)
+               // .whenBecomesTrue(Shooter.INSTANCE.FlywheelOn)
+               // .whenBecomesFalse(Shooter.INSTANCE.FlywheelOff);
 
     }
     @Override public void onUpdate() { }
