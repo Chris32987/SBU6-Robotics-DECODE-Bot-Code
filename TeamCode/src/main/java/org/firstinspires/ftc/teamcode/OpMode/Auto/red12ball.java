58c943eac2d04940e3cea6a7b053366b5f93a0c7
chris.32987 <ingrassiachristopher@gmail.com>
2026-04-09 10:56:12 +0100

    added red 12 ball

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpMode/Auto/red12ball.java
@@ -0,0 +1,11 @@
+package org.firstinspires.ftc.teamcode.OpMode.Auto;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+import org.firstinspires.ftc.teamcode.OpMode.Auto.Paths.TwelveBall;
+import org.firstinspires.ftc.teamcode.Poses;
+
+@Autonomous(name="12 Ball Autonomous - RED")
+public class red12ball extends TwelveBall {
+    public red12ball() {super(Poses.AllianceColor.BLUE); }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpMode/Auto/red18ball.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpMode/Auto/red18ball.java
@@ -5,7 +5,7 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import org.firstinspires.ftc.teamcode.OpMode.Auto.Paths.EighteenBall;
 import org.firstinspires.ftc.teamcode.Poses;
 
-@Autonomous(name="18 Ball Autonomous - red")
+@Autonomous(name="18 Ball Autonomous - RED")
 public class red18ball extends EighteenBall {
     public red18ball() { super(Poses.AllianceColor.RED); }
 }
